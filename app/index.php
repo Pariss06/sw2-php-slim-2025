2decd5df430a7e24916e41dd4f8a25ab61f6fa09
Pariss06 <willianparis834@gmail.com>
2025-03-20 01:50:30 +0000

    Aula 19/03/2025

--- app/index.php
+++ app/index.php
@@ -1,8 +1,17 @@
 <?php
-// require './vendor/autoload.php';
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Slim\View\PhpRenderer;
 
-// use Application\DBConnection\MySQLConnection;
+require __DIR__ . '/vendor/autoload.php';
 
-// $db = new MySQLConnection();
+$app = AppFactory::create();
 
-echo phpinfo();
+$app ->get('/', function(Request $request, Response $response, $args){$response->getBody()->write("Olá Mundo!!!"); return $response; });
+
+$app ->get('/mensagem/{nome}', function(Request $request, Response $response, $args){$response->getBody()->write("Boa Noite!!!" . $args['nome']); return $response; });
+
+$app ->get('hello', function(Request $request, Response $response, $args){$renderer = new PhpRenderer(__DIR__ . '/templates'); $msg = 'Nova Mensagem'; return $renderer->render($response, 'hello.php', ['mensagem' => $msg]); });
+
+$app->run();
